d34da99d27875fbea026fe6398e2ad4870a897f7
Matsko <g.matsko@devlegal.ru>
2018-07-31 17:22:54 +0300

    fixes

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ importScripts('sw-toolbox.js');
 
 toolbox.precache(["index.html","css/main.css", "css/media.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('images/*', toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {
+toolbox.router.get('*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
